80decc29eb6d0e13145fc66d355d57d56bf3a0f4
Mirza Hilmi <dev@mrz.my.id>
2024-11-29 11:10:40 +0700

    refactor: remove humid & co sub handler

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import mqtt from "mqtt";
 import promclient from "prom-client";
-// @deno-types="npm:@types/express@^5.0.0"
+// @deno-types="@types/express"
 import express from "express";
 import { Buffer } from "node:buffer";
 
@@ -32,45 +32,21 @@ const tempGauge = new promclient.Gauge({
     name: "temperature",
     help: "Temperature Gauge",
 });
-const humidGauge = new promclient.Gauge({
-    name: "humidity",
-    help: "Humidity Gauge",
-});
-const carbonGauge = new promclient.Gauge({
-    name: "co",
-    help: "Carbon Gauge",
-});
 
 const tempHandler = (message: Buffer) => {
     const temp = Number(message.toString());
     tempGauge.set(temp);
 };
-const humidHandler = (message: Buffer) => {
-    const humid = Number(message.toString());
-    humidGauge.set(humid);
-};
-const carbonHandler = (message: Buffer) => {
-    const carbon = Number(message.toString());
-    carbonGauge.set(carbon);
-};
 
 client.on("message", (topic, message, _) => {
     console.log(`Message received: ${topic}: ${message.toString()}`);
-    switch (topic) {
-        case "icnss/temperature":
-            tempHandler(message);
-            break;
-        case "icnss/humidity":
-            humidHandler(message);
-            break;
-        case "icnss/co":
-            carbonHandler(message);
-            break;
-    }
+    tempHandler(message);
+    // ...
 });
 
 const app = express();
 app.get("/metrics", async (_, res) => {
-    res.status(200).json(await promclient.register.getMetricsAsJSON());
+    res.set("Content-Type", promclient.register.contentType)
+        .send(await promclient.register.metrics());
 });
 app.listen(3000);
