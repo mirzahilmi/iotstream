5baf05cbaba5cb1969b33977dbaa5640f2d1b423
Komang David <91604889+kmdavidds@users.noreply.github.com>
2024-11-29 14:21:53 +0700

    feat: add filter for each topic

--- main.ts
+++ main.ts
@@ -32,16 +32,44 @@ const tempGauge = new promclient.Gauge({
     name: "temperature",
     help: "Temperature Gauge",
 });
+const humiGauge = new promclient.Gauge({
+    name: "humidity",
+    help: "Humidity Gauge",
+});
+const coGauge = new promclient.Gauge({
+    name: "co2",
+    help: "CO2 Gauge",
+});
 
 const tempHandler = (message: Buffer) => {
     const temp = Number(message.toString());
     tempGauge.set(temp);
 };
+const humiHandler = (message: Buffer) => {
+    const humi = Number(message.toString());
+    humiGauge.set(humi);
+};
+const coHandler = (message: Buffer) => {
+    const co = Number(message.toString());
+    coGauge.set(co);
+};
 
 client.on("message", (topic, message, _) => {
     console.log(`Message received: ${topic}: ${message.toString()}`);
-    tempHandler(message);
-    // ...
+    switch (topic) {
+        case "icnss/temperature":
+            tempHandler(message);
+            break;
+        case "icnss/humidity":
+            humiHandler(message)
+            break;
+        case "icnss/co":
+            coHandler(message)
+            break;
+    
+        default:
+            break;
+    }
 });
 
 const app = express();
